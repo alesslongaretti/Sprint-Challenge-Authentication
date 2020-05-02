31e79d6178628164e0aa4744b8b4bbe260438fc0
alesslongaretti <alesslongaretti@gmail.com>
2020-05-01 21:23:50 -0400

    setup server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const jwt = require('jsonwebtoken');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -14,5 +15,28 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+  });
+
+  server.get('/token', (req,res) => {
+
+    const payload = {
+        subject: 'thisuser',
+        userid: 'aless',
+        favoriteFood: 'peruvian'
+    };
+
+    const secret = 'mysecret';
+
+    const options = {
+        expiresIn: '30m'
+    };
+
+    const token = jwt.sign(payload, secret, options);
+
+    res.json(token);
+})
 
 module.exports = server;
